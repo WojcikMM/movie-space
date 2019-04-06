076e7c90b108d546fa4859d5f86d957ceb1394ff
Michal <m.mwojcik@outlook.com>
2019-04-06 23:20:39 +0200

    Changes styles of Movie Cards & add other animation on rating bar.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,8 +5,7 @@ import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
 
