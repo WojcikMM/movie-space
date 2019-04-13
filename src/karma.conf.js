484dc7d5cd7bdbe81409e67960cffc0b66c52b2e
Michal <m.mwojcik@outlook.com>
2019-04-13 15:33:02 +0200

    Enable Phantom Test CI/CD

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -6,26 +6,30 @@ module.exports = function (config) {
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      require( 'karma-jasmine' ),
-      require( 'karma-chrome-launcher' ),
-      require( 'karma-phantomjs-launcher' ),
-      require( 'karma-remap-istanbul' ),
-      require( 'angular-cli/plugins/karma' )
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+
+      require('karma-phantomjs-launcher'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
-    client: {
+    client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly'],
+      reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
+    },
+    angularCli: {
+      environment: 'dev'
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: [ 'PhantomJS' ],
-    singleRun: false
+    browsers: ['PhantomJS'],
+    singleRun: true
   });
 };
